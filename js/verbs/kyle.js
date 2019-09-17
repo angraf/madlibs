592b04c96f92ff45b440b44c47d6f87a937495e5
angraf <55407991+angraf@users.noreply.github.com>
2019-09-17 15:31:05 -0400

    Update kyle.js

--- js/verbs/kyle.js
+++ js/verbs/kyle.js
@@ -2,4 +2,12 @@ verbs = verbs.concat([
     'run',
     'jump',
     'code'
-]);
+    'kyle-giggle'
+    'mansplain'
+    'womansplain'
+    'manterrupt'
+    'womanterrupt'
+    'forked'
+    'spooned'
+    'de-ambiguified'
+    ]);
